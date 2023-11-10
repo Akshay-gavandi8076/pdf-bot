7699effc60cc49c253e90dd164d7267fd5afdb5b
Akshay-gavandi8076 <Akshay.gavandi1@gmail.com>
2023-11-10 14:41:50 +0100

    remove unused files

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import {
   getKindeServerSession,
 } from '@kinde-oss/kinde-auth-nextjs/server'
 import { ArrowRight } from 'lucide-react'
-import UserAccountNav from './UserAccountNav'
+// import UserAccountNav from './UserAccountNav'
 import MobileNav from './MobileNav'
 
 const Navbar = () => {
@@ -26,7 +26,7 @@ const Navbar = () => {
             <span>quill.</span>
           </Link>
 
-          <MobileNav isAuth={!!user} />
+          {/* <MobileNav isAuth={!!user} /> */}
 
           <div className='hidden items-center space-x-4 sm:flex'>
             {!user ? (
@@ -67,7 +67,7 @@ const Navbar = () => {
                 >
                   Dashboard
                 </Link>
-                <UserAccountNav
+                {/* <UserAccountNav
                   name={
                     !user.given_name || !user.family_name
                       ? 'Your Account'
@@ -75,7 +75,7 @@ const Navbar = () => {
                   }
                   email={user.email ?? ''}
                   imageUrl={user.picture ?? ''}
-                />
+                /> */}
               </>
             )}
           </div>
